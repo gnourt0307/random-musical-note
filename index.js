e41f047e83bea9b53407470e56a99e1384f40e29
gnourt0307 <truongtkls8@gmail.com>
2025-12-30 22:37:16 +0700

    adding generate single note feature

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector(".start-btn");
 const downBtn = document.querySelector(".down-btn");
 const upBtn = document.querySelector(".up-btn");
+const singleNoteBtn = document.querySelector(".single-note-btn");
 const notesArr = [
   "A",
   "A#/Bb",
@@ -16,6 +17,9 @@ const notesArr = [
   "G#/Ab",
 ];
 
+let tempArr1 = [...notesArr];
+let tempArr2 = [...notesArr];
+
 let isRunning = false;
 let intervalId;
 
@@ -25,21 +29,24 @@ startBtn.addEventListener("click", () => {
     startBtn.innerText = "Stop";
     downBtn.disabled = true;
     upBtn.disabled = true;
+    singleNoteBtn.disabled = true;
     const currentSecondEle = document.getElementById("second");
     const currentSecond = Number(currentSecondEle.innerText);
     intervalId = setInterval(() => {
-      const randomNumber = Math.round(Math.random() * 11);
-      const note = notesArr[randomNumber];
-      let currentNote = document.getElementById("note");
-      currentNote.innerText = note;
+      randomNoteGenerate(tempArr1);
     }, currentSecond * 1000);
   } else {
     isRunning = false;
     downBtn.disabled = false;
     upBtn.disabled = false;
+    singleNoteBtn.disabled = false;
     startBtn.innerText = "Start";
     clearInterval(intervalId);
   }
+});
+
+singleNoteBtn.addEventListener("click", () => {
+  randomNoteGenerate(tempArr2);
 });
 
 downBtn.addEventListener("click", () => {
@@ -70,3 +77,15 @@ upBtn.addEventListener("click", () => {
   if (newSecond >= 2) secondText.innerText = "seconds";
   else secondText.innerText = "second";
 });
+
+function randomNoteGenerate(arr) {
+  if (arr.length === 0) {
+    arr.push(...notesArr);
+  }
+  console.log(arr);
+  const randomNumber = Math.round(Math.random() * (arr.length - 1));
+  const note = arr[randomNumber];
+  let currentNote = document.getElementById("note");
+  currentNote.innerText = note;
+  arr.splice(randomNumber, 1);
+}
